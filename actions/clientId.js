af7716811f7c5649aa526d7711c638f1ba5b46b1
Jordon Leach <40806497+jordonleach@users.noreply.github.com>
2018-12-12 12:13:49 -0500

    Delete clientId.js

--- actions/clientId.js
+++
@@ -1,6 +0,0 @@
-export const ANDROID_CLIENT_ID = 
-	"465052148625-23u35g86vblqup4khfpsp7l113je11c5.apps.googleusercontent.com";
-
-export const IOS_CLIENT_ID = 
-	"465052148625-8mqg1m3d4cv6vlq195e6phs6gd3ghej6.apps.googleusercontent.com";
-
